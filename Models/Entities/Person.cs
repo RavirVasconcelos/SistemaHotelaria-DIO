4a392a4f028be82b512d244f702bee84b6b16ffd
Ytalo Vasconcelos <ytalovasconcelos01@gmail.com>
2025-04-30 22:26:10 -0300

    Finalizado

--- Models/Entities/Person.cs
+++ Models/Entities/Person.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SistemaHotelaria.Models.Entities.Enums;
+﻿using SistemaHotelaria.Models.Entities.Enums;
 
 namespace SistemaHotelaria.Models.Entities
 {
--- Models/Entities/Reservation.cs
+++ Models/Entities/Reservation.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SistemaHotelaria.Models.Entities.Enums;
+﻿using System.Text;
 using SistemaHotelaria.Models.Exception;
 
 namespace SistemaHotelaria.Models.Entities
